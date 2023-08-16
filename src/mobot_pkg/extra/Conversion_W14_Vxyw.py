a8742273cebd8e918e3c65c70a4df954ef32e4d2
shihab28 <99267473+shihab28@users.noreply.github.com>
2023-08-16 10:59:35 -0400

    Update Conversion_W14_Vxyw.py

--- src/mobot_pkg/extra/Conversion_W14_Vxyw.py
+++ src/mobot_pkg/extra/Conversion_W14_Vxyw.py
@@ -1,1 +1,11 @@
 
+def cmdvel2Vxyw(self):
+  Vx = cmdvel.linear.x
+  Vy = cmdvel.linear.y
+  W0 = cmdvel.angular.z
+  print(Vx, Vy, W0)
+
+def Angular2Vxyw(self):
+  Vx = (W1 + W2 + W3 + W4) * R / 4
+  Vy = (W1 - W2 - W3 + W4) * R / 4
+  W0 = (-W1*LS + W2*LS - W3*LS + W4*LS) * R / 4
