7e7b1e6f74adf8e136342bd47517122963d6d064
shihab28 <shihab.ahamad28@gmail.com>
2024-01-02 10:46:25 -0500

    .

--- src/mobot_pkg/extra/extra.py
+++ src/mobot_pkg/extra/extra.py
@@ -1,6 +1,11 @@
-vy_samples = []
-for vals in range(700,1401,50):
-    vy_samples.append(vals/1000)
-    vy_samples.append(-vals/1000)
+# vy_samples = []
+# for vals in range(700,1401,50):
+#     vy_samples.append(vals/1000)
+#     vy_samples.append(-vals/1000)
 
-print(vy_samples)
+# print(vy_samples)
+
+
+import cv2
+print(cv2.__version__)
+print(cv2.getBuildInformation())
