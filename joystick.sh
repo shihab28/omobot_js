0967b206bc92c508bbd30f38a393d7e384ef9555
shihab28 <shihab.ahamad28@gmail.com>
2024-01-02 11:18:16 -0500

    .

--- joystick.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# ls /dev/input/
-# sudo jstest /dev/input/js0
-# ls -l /dev/input/js0
-# sudo chmod a+rw /dev/input/js0
-rosparam set joy_node/dev "/dev/input/js0"
-rosrun joy joy_node #
-# rostopic echo joy #new terminal
