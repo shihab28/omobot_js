96065209b6f4aa30ee091b353dc4b39fae30950a
Shihab28 <shihab.ahamad28@gmail.com>
2024-03-20 16:46:48 -0400

    omobot

--- src/mobot_pkg/arduino/EncoderReading/EncoderReading.ino
+++ src/mobot_pkg/arduino/EncoderReading/EncoderReading.ino
@@ -1,127 +1,97 @@
 /*
-   rosserial::geometry_msgs::PoseArray Test
-   Sums an array, publishes sum
+    Omobot's Wheel Speed and Power Monitoring
+    Author: Shihab Uddin Ahamad
+    Email: shihab.ahamad28@gmail.com
+    Date: 03/20/2024
+    Description: 
+    - This code is designed for Omobot with encoders on each wheel for getting rotational speed of the wheel, and an ACS712 sensor for power monitoring. 
+    - It calculates and send the speed of each wheel, battery voltage and current as string data over serial communication to Jetson Nano.
+
+    Hardware Connections:
+    - Encoders connected to digital pins 4 through 11
+    - ACS712 sensor for current measurement connected to A1
+    - Battery voltage measurement connected to A0
+
+    Note: This code is specifically tailored for Arduino boards and requires the ACS712 Arduino library.
 */
-#include <Wire.h>
-#include "ACS712.h"
 
+#include "ACS712.h"  // Include library for ACS712 current sensor
 
+String encoder_msg = "";  // String to store encoder messages
 
-
-//#include <ros.h>
-//#include <std_msgs/String.h>
-//#include <geometry_msgs/Pose.h>
-//#include <geometry_msgs/PoseArray.h>
-
-//ros::NodeHandle nh;
-//bool set_;
-//std_msgs::String str_msg;
-//ros::Publisher p("encoder_feedback", &str_msg);
-String encoder_msg = "";
-
+// Define digital pins for encoder connections
 #define pinEncRF2 4
 #define pinEncRF1 5
-
 #define pinEncLF1 6
 #define pinEncLF2 7
 #define pinEncLB1 8
 #define pinEncLB2 9
-
 #define pinEncRB2 10
 #define pinEncRB1 11
 
-
+// Define analog pins for battery voltage and current measurements
 #define pinBatteryVol A0
 #define pinBatteryCur A1
 
-
-
+// Define constants for calculations
 #define R 248.18582;
 #define pi 3.1416;
 
+// Variable declarations for encoder and speed calculations
 int i, j = 0, l, w = 0;
 long pulseRF, pulseLF, pulseRB, pulseLB;
 long prevPulseRF = 0, prevPulseLF = 0, prevPulseRB = 0, prevPulseLB = 0;
 int speedRF, speedLF, speedRB, speedLB;
 int prevSpeedRF = 0, prevSpeedLF = 0, prevSpeedRB = 0, prevSpeedLB = 0;
 long prevTime = 0, curTime, delTime;
-int ppr1 = 8544, ppr2 = 8576, ppr3 = 8544, ppr4 = 8576;
-float mmpp1 = 1, mmpp2 = 1, mmpp3 = 1, mmpp4 = 1;
+int ppr1 = 8544, ppr2 = 8576, ppr3 = 8544, ppr4 = 8576;  // Pulses per revolution for each encoder
+float mmpp1 = 1, mmpp2 = 1, mmpp3 = 1, mmpp4 = 1;  // Millimeters per pulse
 
-//void messageCb(const geometry_msgs::PoseArray& msg){
-//  sum_msg.position.x = 0;
-//  sum_msg.position.y = 0;
-//  sum_msg.position.z = 0;
-//  for(int i = 0; i < msg.poses_length; i++)
-//  {
-//    sum_msg.position.x += msg.poses[i].position.x;
-//    sum_msg.position.y += msg.poses[i].position.y;
-//    sum_msg.position.z += msg.poses[i].position.z;
-//  }
-//  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-//}
-
-//ros::Subscriber<geometry_msgs::PoseArray> s("poses",messageCb);
-
+// Initialize ACS712 current sensor
 ACS712  ACS(pinBatteryCur, 3.3, 1023, 520);
 
+// Function to set pin modes for encoder inputs
 void pinModeDef() {
   for (i = 4; i < 12; i++) {
-    pinMode(i, INPUT_PULLUP);
+    pinMode(i, INPUT_PULLUP);  // Set encoder pins as input with pull-up
   }
 }
-//void pinModeDef1() {
-//  for (i = 0; i < 4; i++) {
-//    pinMode(pinIntV + i, OUTPUT);
-//  }
-//}
 
-
-
-void setup()
-{
-//  mmpp1 = 248.18582 / 8544;
-//  mmpp2 = 248.18582 / 8576;
-//  mmpp3 = 248.18582 / 8544;
-//  mmpp4 = 248.18582 / 8576;
+// Setup function runs once at the start
+void setup() {
+  pinModeDef();  // Set pin modes for encoders
+  pinMode(pinBatteryVol, INPUT);  // Set pin mode for battery voltage
+  pinMode(pinBatteryCur, INPUT);  // Set pin mode for battery current
   
-  pinModeDef();
-  pinMode(pinBatteryVol, INPUT);
-  pinMode(pinBatteryCur, INPUT);
-  //  pinModeDef1();
-  Serial1.begin(57600);
-  Serial.begin(115200);
+  Serial1.begin(57600);  // Begin Serial1 communication
+  Serial.begin(115200);  // Begin Serial  communication for debugging
+  
+  // Attach interrupt service routines for encoder signal changes
   attachInterrupt(digitalPinToInterrupt(pinEncRF1), Change_RF, CHANGE);
   attachInterrupt(digitalPinToInterrupt(pinEncLF1), Change_LF, CHANGE);
   attachInterrupt(digitalPinToInterrupt(pinEncRB1), Change_RB, CHANGE);
   attachInterrupt(digitalPinToInterrupt(pinEncLB1), Change_LB, CHANGE);
 
-
-  ACS.autoMidPoint();
-//  nh.initNode();
-  //  nh.subscribe(s);
-//  nh.advertise(p);
+  ACS.autoMidPoint();  // Calibrate the zero current point
 }
 
-int delayTime = 25;
-int voltage = 0, voltageAvg = 0, current = 0, currentAvg = 0, curMidValue = 508, curMultiplier = 100;
-int cnt = 0, sample = 4;
-int cnt_cur = 0, sample_cur = 40;
-int cnt_vol = 0, sample_vol = 20;
-void loop()
-{
-  curTime = millis();
-  delTime = curTime - prevTime;
+// Main loop function runs repeatedly
+void loop() {
+  curTime = millis();  // Get current time
+  delTime = curTime - prevTime;  // Calculate time difference
+  
+  // Calculate speeds for each wheel
   speedLF = (pulseLF - prevPulseLF) * mmpp1 * 1000 / delTime;
   speedRF = (pulseRF - prevPulseRF) * mmpp2 * 1000 / delTime;
   speedLB = (pulseLB - prevPulseLB) * mmpp3 * 1000 / delTime;
   speedRB = (pulseRB - prevPulseRB) * mmpp4 * 1000 / delTime;
 
-//  int mA = ACS.mA_DC();
+  // Measure battery current
   int mA = (analogRead(pinBatteryCur) - curMidValue) * 1000 * 3.33 / (curMultiplier * 5);
   
+  // Average current over several samples for stability
   if (cnt_cur < sample_cur){
-    current = current + mA;
+    current += mA;
     cnt_cur++;
   }
   else{
@@ -129,8 +99,10 @@ void loop()
     cnt_cur = 0;
     current = 0;
   }
+  
+  // Average voltage over several samples for stability
   if (cnt_vol < sample_vol){
-    voltage = voltage + analogRead(pinBatteryVol);
+    voltage += analogRead(pinBatteryVol);
     cnt_vol++;
   }
   else{
@@ -139,28 +111,29 @@ void loop()
     voltage = 0;
   }
 
-  prevPulseLF = pulseLF; prevPulseRF = pulseRF; prevPulseLB = pulseLB; prevPulseRB = pulseRB; 
-  prevTime = curTime;
+  // Update previous pulse counts and time
+  prevPulseLF = pulseLF; prevPulseRF = pulseRF; prevPulseLB = pulseLB; prevPulse
   
+  prevTime = curTime; // Set current time as past time for next loop
+  
+  // Create String with the speed of the four wheels, current and voltage readings for sending over Serial1
   encoder_msg = String(speedLF) + "," + String(speedRF) + "," + String(speedLB) + "," + String(speedRB)+ "," + String(voltageAvg) + "," + String(currentAvg);
   
-//  int msg_len = encoder_msg.length() + 1;
-//  char encoderMsgArray[msg_len];
-//  encoder_msg.toCharArray(encoderMsgArray, msg_len);
+  // int msg_len = encoder_msg.length() + 1;
+  // char encoderMsgArray[msg_len];
+  // encoder_msg.toCharArray(encoderMsgArray, msg_len);
+
+  // Send the String message over Serial1 and print in Serial port for debugginh
   Serial1.println(encoder_msg);
   Serial.println(encoder_msg);
+
 
   delay(delayTime);
 }
 
 
 
-
-
-
-
-
-
+// Update the pulses interrrupted the the encoder of the right-forward wheel
 void Change_RF() {
   if (digitalRead(pinEncRF2) == 0) {
     if (digitalRead(pinEncRF1) == 0) {
@@ -177,7 +150,7 @@ void Change_RF() {
   }
 }
 
-
+// Update the pulses interrrupted the the encoder of the left-forward wheel
 void Change_LF() {
   if (digitalRead(pinEncLF2) == 0) {
     if (digitalRead(pinEncLF1) == 0) {
@@ -194,6 +167,7 @@ void Change_LF() {
   }
 }
 
+// Update the pulses interrrupted the the encoder of the right-back wheel
 void Change_RB() {
   if (digitalRead(pinEncRB2) == 0) {
     if (digitalRead(pinEncRB1) == 0) {
@@ -210,7 +184,7 @@ void Change_RB() {
   }
 }
 
-
+// Update the pulses interrrupted the the encoder of the left-back wheel
 void Change_LB() {
   if (digitalRead(pinEncLB2) == 0) {
     if (digitalRead(pinEncLB1) == 0) {
@@ -226,3 +200,6 @@ void Change_LB() {
     }
   }
 }
+
+
+
--- src/mobot_pkg/arduino/MotorMovement/MotorMovement.ino
+++ src/mobot_pkg/arduino/MotorMovement/MotorMovement.ino
@@ -1,15 +1,38 @@
-//#include <ros.h>
-//#include <geometry_msgs/Twist.h>
-//#include <std_msgs/Int16MultiArray.h>
-//ros::NodeHandle nh;
-//std_msgs::Int16MultiArray wheel_pwm
+/*
+    Omobot's Motor's Pulse Width Modulation Controller
+    Author: Shihab Uddin Ahamad
+    Email: shihab.ahamad28@gmail.com
+    Date: 03/20/2024
+    Description: 
+    - This code is designed for controlling the motors of the Omobot using PWM control 
+    - It receives the speed of each wheel as string data over serial communication from Jetson Nano and sends the 8-bit PWM pulses to L298N Motor driver
+
+    Hardware Connections:
+    - Motor controller connected to digital pins 2 through analog pin A1
+
+    Note: This code is specifically tailored for Arduino boards and requires the ACS712 Arduino library.
+*/
 
 
-#include "protothreads.h"
-#include "StringSplitter.h"
-//#include <StringAction.h>
-//StringAction pwmReader;
+/*
+    Omobot's Motor's Pulse Width Modulation Controller
+    Author: Shihab Uddin Ahamad
+    Email: shihab.ahamad28@gmail.com
+    Date: 03/20/2024
+    Description: 
+    - This code is designed for controlling the motors of the Omobot using PWM control 
+    - It receives the speed of each wheel as string data over serial communication from Jetson Nano and sends the 8-bit PWM pulses to L298N Motor driver
 
+    Hardware Connections:
+    - Motor controller connected to digital pins 2 through analog pin A1
+
+    Note: This code is specifically tailored for Arduino boards and requires the ACS712 Arduino library.
+*/
+
+#include "protothreads.h"  // Include the ProtoThreads library for lightweight threading
+#include "StringSplitter.h"  // Include the StringSplitter library for parsing received serial data
+
+// Define protothread objects for running motor control and other functions
 pt runMot1;
 pt runMot2;
 pt runMot3;
@@ -17,143 +40,78 @@ pt runMot4;
 pt brakeMot;
 pt lightBink;
 
+// Define pin assignments for motor control
 #define pinMotRFpwm 2
 #define pinMotRF1 3
 #define pinMotRF2 4
-
 #define pinMotLF2 5
 #define pinMotLFpwm 6
 #define pinMotLF1 7
-
-#define pinMotLB2   8
+#define pinMotLB2 8
 #define pinMotLBpwm 9
-#define pinMotLB1   10
-
+#define pinMotLB1 10
 #define pinMotRBpwm 11
-#define pinMotRB2   A0
-#define pinMotRB1   A1
+#define pinMotRB2 A0
+#define pinMotRB1 A1
 
-#define statusPin 13
+#define statusPin 13  // Pin for status indication (blinking LED)
 
+// Variable declarations for wheel speed control and kinematics calculations
 int W1, W2, W3, W4;
-float Ln  = .1185, Wd  = .0825;
-float SL = sqrt(Ln * Ln + Wd*Wd), r  = .0398, R  = 2 * 3.14159 * r;
-float LS = 1 / SL;
-int i, j, k, l, n;
-int min_motor_speed = 30;
-int runSequence = {0, 1, 2, 3};
-//void cmdVelCallback(const geometry_msgs::Twist& cmd_vel_msg) {
+int min_motor_speed = 30;  // Minimum motor speed for operation
+int runSequence[] = {0, 1, 2, 3};  // Motor run sequence for alternative coordinated movement
 
-//void cmdVelCallback(const std_msgs::Int16MultiArray& wheel_pwm_msg) {
-////  float Vx = cmd_vel_msg.linear.x;
-////  float Vy = cmd_vel_msg.linear.y;
-////  float W0 = cmd_vel_msg.angular.z;
-//
-//    W1 = wheel_pwm_msg.data[0];
-//    W2 = wheel_pwm_msg.data[1];
-//    W3 = wheel_pwm_msg.data[2];
-//    W4 = wheel_pwm_msg.data[3];
-//
-//  checkMaxSpeed();
-//  if (abs(W1) >= min_motor_speed || abs(W2) >= min_motor_speed || abs(W3) >= min_motor_speed || abs(W4) >= min_motor_speed){
-//  runAllMotorFromSpeed();
-//  }
-//  else{
-//    brake();
-//  }
-//}
-//ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", cmdVelCallback);
-//ros::Subscriber<std_msgs::Int16MultiArray> cmd_vel_sub("wheel_pwm", cmdVelCallback);
-
-
+// Function to initialize all defined pins as output
 void initPinModeOutput(int pinFirst, int pinLast) {
   for (i = pinFirst; i <= pinLast; i++) {
     pinMode(i, OUTPUT);
   }
   pinMode(A0, OUTPUT);
   pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
   pinMode(statusPin, OUTPUT);
 }
 
-
-
-
-
-
-
-
-
+// Setup function for initial configurations
 void setup() {
-  Serial.begin(230400);
-  //  Serial1.begin(57600);
-  initPinModeOutput(2, 11);
-
-  //  nh.initNode();
-  //  nh.subscribe(cmd_vel_sub);
+  Serial.begin(230400);       // Begin serial communication with high baud rate for receiving data from Jetson Nano
+  initPinModeOutput(2, 11);  // Initialize motor control pins as output
 }
 
-
-
-int refreshDelay = 10;
-String splitedString[4] = {"0", "0", "0", "0"};
-int pwmSpeed[4] = {0, 0, 0, 0};
-String serialData;
-String tempStr;
-int itemCount;
+// Main loop that continuously checks for new serial data and controls motors accordingly
 void loop() {
-  //  nh.spinOnce();
-
   while (!Serial.available()) {
-//    delay(10);
+    // Wait for incoming serial data
   }
   
   if (Serial.available()) {
-    serialData  = Serial.readStringUntil('\n');
-    serialData.trim();
-//    Serial1.println(serialData);
-      
-    PT_SCHEDULE(blinkLED(&lightBink));
-    SplitString();
-
+    serialData = Serial.readStringUntil('\n');  // Read the incoming data until newline character
+    serialData.trim();  // Trim whitespace from the data
     
-//    StringSplitter *splitter = new StringSplitter(serialData, ',', 4);
-//    itemCount = splitter->getItemCount();
-//    if (itemCount == 4) {
-//      for (i = 0; i < itemCount; i++) {
-//        tempStr = splitter->getItemAtIndex(i);
-//        pwmSpeed[i] = tempStr.toFloat();
-//      }
-//      
-//    }
-//
-//    else {
-//      pwmSpeed[0] = 0; pwmSpeed[1] = 0; pwmSpeed[2] = 0; pwmSpeed[3] = 0;
-//      brake();
-//    }
-
-    if (abs(pwmSpeed[0]) < 50 && abs(pwmSpeed[1]) < 50 && abs(pwmSpeed[2]) < 50 && abs(pwmSpeed[3]) < 50){
+    PT_SCHEDULE(blinkLED(&lightBink));  // Blink the status LED to indicate data reception
+    SplitString();  // Split the received string into individual speed values for each motor
+    
+    // Check if the received speeds are below a certain threshold and if so, brake
+    if (abs(pwmSpeed[0]) < 50 && abs(pwmSpeed[1]) < 50 && abs(pwmSpeed[2]) < 50 && abs(pwmSpeed[3]) < 50) {
       brake();
-    }
-    else{
+    } else {
+      // If speeds are above the threshold, control motors based on received speeds
       runAllMotorFromSpeed();
     }
-    
-    delay(refreshDelay);
+
+    delay(refreshDelay);  // Short delay to allow for processing time and prevent flooding
   }
-
-
-
 }
 
+// Function to split the received string into individual motor speed values
+void SplitString() {
+  const int maxSplitParts = 4;  // Maximum number of parts to split the string into (corresponding to 4 motors)
+  String splitParts[maxSplitParts];  // Array to hold the split string parts
+  int splitCount = 0;  // Counter for the number of parts split
+  int lastIndex = 0;  // Index of the last splitted part
 
-void SplitString(){
-  const int maxSplitParts = 4; 
-  String splitParts[maxSplitParts]; 
-  int splitCount = 0; 
-  int lastIndex = 0; 
   
-  while (splitCount < maxSplitParts - 1) {
+  // Split the message with "," seperator, record the number of split and last index
+   while (splitCount < maxSplitParts - 1) {
     int commaIndex = serialData.indexOf(',', lastIndex);
     
     if (commaIndex >= 0) { 
@@ -161,22 +119,26 @@ void SplitString(){
       lastIndex = commaIndex + 1; 
       splitCount++; 
     } else {
-      break; 
+      break;  // Exit loop if no more commas are found
     }
   }
   
+  // Capture the last part of the message if it doesn't end with a comma
   if (lastIndex < serialData.length() && splitCount < maxSplitParts) {
     splitParts[splitCount] = serialData.substring(lastIndex);
     splitCount++; 
   }
 
-  resetPwmSpeed();
+  resetPwmSpeed();  // Reset speeds to 0 before setting new values
+  // Convert split string parts to integers and assign them to pwmSpeed array
   for (int i = 0; i < splitCount; i++) {
     pwmSpeed[i] = splitParts[i].toInt();
   }
 }
 
 
+// Resets the PWM speed values for all motors to 0.
+// This function is typically called before reading new speed values to ensure that outdated speeds are not used.
 void resetPwmSpeed(){
    pwmSpeed[0] = 0; 
    pwmSpeed[1] = 0;
@@ -184,90 +146,61 @@ void resetPwmSpeed(){
    pwmSpeed[3] = 0;
 }
 
+// Blinks the status LED to indicate activity. This can be used as a visual
+// signal for debugging or to show that the system is processing commands.
+// The LED is turned on for 40 milliseconds and then off for 10 milliseconds.
 int blinkLED(struct pt* pt) {
-      digitalWrite(statusPin, HIGH);
-      delay(40);
-      digitalWrite(statusPin, LOW);
-      delay(10);
-//      digitalWrite(statusPin, HIGH);
-//      delay(30);
-//      digitalWrite(statusPin, LOW);
-//      delay(10);
+      digitalWrite(statusPin, HIGH); // Turn the LED on
+      delay(40); // Wait for 40 milliseconds
+      digitalWrite(statusPin, LOW); // Turn the LED off
+      delay(10); // Wait for 10 milliseconds
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Ensures that the speed for each motor does not exceed the maximum allowable
+// value of 255 or go below the minimum of -255. This function acts as a safety
+// check to prevent sending invalid PWM values to the motor driver.
 void checkMaxSpeed() {
-  if (pwmSpeed[0] > 255) {
-    pwmSpeed[0] = 255;
-  } else if (pwmSpeed[0] < -255) {
-    pwmSpeed[0] = -255;
-  }
-  if (pwmSpeed[1] > 255) {
-    pwmSpeed[1] = 255;
-  } else if (pwmSpeed[1] < -255) {
-    pwmSpeed[1] = -255;
-  }
-  if (pwmSpeed[2] > 255) {
-    pwmSpeed[2] = 255;
-  } else if (pwmSpeed[2] < -255) {
-    pwmSpeed[2] = -255;
-  }
-  if (pwmSpeed[3] > 255) {
-    pwmSpeed[3] = 255;
-  } else if (pwmSpeed[3] < -255) {
-    pwmSpeed[3] = -255;
+  // Clamp the speed values for each motor to the range [-255, 255]
+  for (int i = 0; i < 4; i++) {
+    if (pwmSpeed[i] > 255) {
+      pwmSpeed[i] = 255;
+    } else if (pwmSpeed[i] < -255) {
+      pwmSpeed[i] = -255;
+    }
   }
 }
 
+// Runs all motors based on the current speed values stored in the pwmSpeed array.
+// This function iterates through each motor, assigning the respective speed value
+// and adjusting the motor control signals accordingly.
 void runAllMotorFromSpeed() {
-  int tempSeq
-  tempSeq = runSequence;
+  // Temporary sequence array to ensure motors are run in order
+  int tempSeq[4] = {0, 1, 2, 3};
   for (int j = 0; j < 4; j++) {
-    runMotor(runSequence[j], pwmSpeed[j]);
-    tempSeq[j] = (j + 1) % 4;
-    //    Serial.print(pwmSpeed[j]); Serial.print(" | ");
-    //    Serial1.print(pwmSpeed[j]); Serial1.print(" | ");
+    runMotor(tempSeq[j], pwmSpeed[j]); // Run each motor with its designated speed
   }
-  runSequence = tempSeq;
-
-  //  Serial.println("");
-
-  //  runMotor(1, pwmSpeed[1]);
-  //  runMotor(2, pwmSpeed[2]);
-  //  runMotor(3, pwmSpeed[3]);
 }
 
+signed int current_motorSpeed = 0; // Define the speed of the current motor to be controlled
 
-signed int current_motorSpeed = 0;
-
+// Controls the speed and direction of a specific motor based on the given speed.
+// Positive values indicate one direction, while negative values indicate the reverse.
+// This function maps the motor ID to the respective control pins and applies the PWM signal.
 void runMotor(int motorId, signed int motorSpeed) {
-  current_motorSpeed = motorSpeed;
-  if (motorId == 0) {
-    PT_SCHEDULE(runMotorThread1(&runMot1));
-    //    runMotorThread1(&runMot1);
-  } else if (motorId == 1) {
-    PT_SCHEDULE(runMotorThread2(&runMot2));
-    //    runMotorThread2(&runMot2);
-  } else if (motorId == 2) {
-    PT_SCHEDULE(runMotorThread3(&runMot3));
-    //    runMotorThread3(&runMot3);
-  } else if (motorId == 3) {
-    PT_SCHEDULE(runMotorThread4(&runMot4));
-    //    runMotorThread4(&runMot4);
+  current_motorSpeed = motorSpeed; // Store current motor speed for use in thread functions
+  // Schedule the appropriate thread function based on the motor ID
+  switch(motorId) {
+    case 0: PT_SCHEDULE(runMotorThread1(&runMot1)); break;
+    case 1: PT_SCHEDULE(runMotorThread2(&runMot2)); break;
+    case 2: PT_SCHEDULE(runMotorThread3(&runMot3)); break;
+    case 3: PT_SCHEDULE(runMotorThread4(&runMot4)); break;
   }
 }
 
+
+// Thread functions for controlling each motor. These functions set the motor
+// driver pins based on the direction indicated by the current speed (positive or negative).
+// Each function handles a different motor, applying the PWM speed to the designated PWM control pin.
 int runMotorThread1(struct pt* pt) {
   if (current_motorSpeed >= 0) {
     digitalWrite(pinMotLF1, LOW);
@@ -317,6 +250,9 @@ int runMotorThread4(struct pt* pt) {
 }
 
 
+// Engages the braking mechanism for all motors by applying a high signal to both
+// direction control pins of each motor driver. This function is useful for quickly
+// stopping the robot or holding its position against external forces.
 int brakeThread(struct pt* pt) {
   digitalWrite(pinMotLB1, HIGH);
   digitalWrite(pinMotLB2, HIGH);
@@ -332,12 +268,8 @@ int brakeThread(struct pt* pt) {
   analogWrite(pinMotRFpwm, 250);
 }
 
-int TargEncSpeed[4] = {0, 0, 0, 0};
+// Wrapper function to schedule the braking thread. This can be called to initiate
+// braking on all motors.
 void brake() {
-  // Serial.println("Braking");
-  //  TargEncSpeed[0] = 0;
-  //  TargEncSpeed[1] = 0;
-  //  TargEncSpeed[2] = 0;
-  //  TargEncSpeed[3] = 0;
   PT_SCHEDULE(brakeThread(&brakeMot));
 }
